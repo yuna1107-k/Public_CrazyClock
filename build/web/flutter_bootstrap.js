7e21b1d2d41f0a7c13cec4785fb88875fc167c9f
yuna1107-k <main@yuna1107.com>
2026-05-20 00:49:48 +0900

    ver0.1.0 rebuild

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3781274462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1862738703" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
